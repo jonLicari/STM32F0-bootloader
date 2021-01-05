0d6c6ce5d9fd24cd324b5d7dfd4c4851d9825be1
Jonathan Licari <jlicari@ryerson.ca>
2021-01-05 17:08:40 -0500

    Update host.py

--- host/host.py
+++ host/host.py
@@ -3,10 +3,9 @@ import time
 import os
 
 # Constants
-fileName = 'NewFirmware.bin'
+fileName = 'cli.bin'
 portNum = 'COM3'
-SIZE = 128
-sleepTime = 1
+SIZE = 256
 
 # Open data file for r -read b -binary (for images)
 dataFile = open(fileName, 'rb') 
@@ -14,7 +13,7 @@ dataFile = open(fileName, 'rb')
 # Open serial COM port
 port = serial.Serial(
     port = portNum, 
-    baudrate = 9600, 
+    baudrate = 115200, 
     bytesize = serial.EIGHTBITS, 
     parity = serial.PARITY_NONE, 
     stopbits = serial.STOPBITS_ONE
@@ -35,19 +34,16 @@ def main():
         flag = port.read(1) # Read UART 
         if (flag == b'2'): # Only transmit once STM is ready
             print(flag)
-            time.sleep(sleepTime)
+            time.sleep(1)
             # Send the number of packets to be expected
             NumberOfPackets()
             break
-
-    time.sleep(1)
 
     while True:
         flag = port.read(1) # Read UART 
         if (flag == b'1'): # Only transmit once STM is ready
             print(flag)
             flag = 0 # reset ready bit
-            time.sleep(sleepTime)
             transmit()
             ctr+=1
             print(ctr)
@@ -69,30 +65,32 @@ def transmit():
         if (byte == b''):
             global endFlag
             endFlag = 1 # Set end of file flag
-            port.write(b'0xFF')
-            print(b'0xFF')
+            port.write(b'\xFF')
+            print(b'\xFF')
             #break # out of while loop, to transmit()
         else:
             port.write(byte) # Write byte to UART
             print(byte)
+
         index += 1
         
     if (endFlag == 1):
         print("End of file ")
         # Close file 
         dataFile.close()
+
     print("End packet ")
 
 def NumberOfPackets():
     totalBytes = os.stat(fileName).st_size 
     packetSize = totalBytes//SIZE
-
+    
     if (totalBytes%SIZE == 0): # File size perfectly divisible by packet size
         port.write(str(packetSize).encode())
         print(str(packetSize).encode())
     else:
         port.write(str(packetSize+1).encode())
         print(str(packetSize+1).encode())
-
+    
 if __name__ == "__main__":
     main()
