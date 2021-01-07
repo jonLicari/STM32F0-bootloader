a25992ac3e3136e019fa0deee7b8bb26dce12021
Jonathan Licari <jlicari@ryerson.ca>
2021-01-07 11:35:06 -0500

    Update main.c

--- STM32/main.c
+++ STM32/main.c
@@ -33,11 +33,18 @@
 
 /* Private define ------------------------------------------------------------*/
 /* USER CODE BEGIN PD */
-#define FLASH_BASE_ADDR ((uint32_t)0x8000000)
-#define FLASH_USR_ADDR ((uint32_t)0x08005000)
-#define CALLOC_SIZE 128
-#define PCK_LEN 3
-#define SWAP_INT32(x) (((x) >> 24) | (((x) & 0x00FF0000) >> 8) | (((x) & 0x0000FF00) << 8) | ((x) << 24))
+#define FLASH_BASE_ADDR ((uint32_t) 0x8000000) // Base address of flash memory
+#define FLASH_USR_ADDR ((uint32_t) 0x08005000) // Base address of user app flash memory
+#define MAX_FLASH_PAGES 64 // Maximum number of pages of flash memory
+#define CALLOC_SIZE 256 // Number of bytes per data packet
+#define PCK_LEN 2
+#define VT_SIZE 48 	// Number of elements in interrupt vector table
+#define USER_BTN_PORT B1_GPIO_Port
+#define USER_BTN_PIN B1_Pin
+#define BLUE_LED_PIN GPIO_PIN_8
+#define BLUE_LED_PORT GPIOC
+#define GREEN_LED_PIN GPIO_PIN_9
+#define GREEN_LED_PORT GPIOC
 /* USER CODE END PD */
 
 /* Private macro -------------------------------------------------------------*/
@@ -50,11 +57,11 @@ UART_HandleTypeDef huart1;
 
 /* USER CODE BEGIN PV */
 #if defined ( __GNUC__ )
-__IO uint32_t VectorTable[48] __attribute__((section(".RAMVectorTable")));
+__IO uint32_t VectorTable[VT_SIZE] __attribute__((section(".RAMVectorTable")));
 #endif
 
 uint8_t rxByte = 0; // Receives file data
-uint8_t rxCplt = 0;
+uint8_t rxCplt = 0; // Receive byte transaction complete flag
 static uint16_t rxIndex = 0; // Receive buffer index
 static uint32_t packetExpect = 0;
 static uint16_t packetIndex = 0;
@@ -71,10 +78,10 @@ static void MX_USART1_UART_Init(void);
 /* USER CODE BEGIN PFP */
 void VectorTableRelocate(void);
 void PeripheralDeInit(void);
-void BLJumpToUsrApp(void);
+void BLJumpToUserApp(void);
 void EraseFlashApp(void);
 static inline uint32_t PagesToErase(void);
-static inline uint32_t TotalPack(void);
+static inline uint32_t TotalPacket(void);
 void WriteToFlash (uint16_t index);
 
 /* USER CODE END PFP */
@@ -91,17 +98,21 @@ void WriteToFlash (uint16_t index);
 int main(void)
 {
   /* USER CODE BEGIN 1 */
-  char ready[] = "1";
-  char begin[] = "2";
+  char ready[] = "1"; // Ready for next transaction flag
+  char begin[] = "2"; // Begin file transfer flag
   char tx[20] = {0};
 
-  // Allocate RAM buffer
+  // Allocate RAM buffer in heap to survive beyond the scope of main
   ram = (char*) calloc(CALLOC_SIZE, sizeof(char));
 
   if (ram == NULL)
+  {
   	sprintf(tx, "Allocate Err");
+  }
   else
+  {
 	sprintf(tx, "0x%lX \r\n", (uint32_t)ram);
+  }
 
   /* USER CODE END 1 */
 
@@ -118,7 +129,6 @@ int main(void)
   SystemClock_Config();
 
   /* USER CODE BEGIN SysInit */
-  VectorTableRelocate();
   /* USER CODE END SysInit */
 
   /* Initialize all configured peripherals */
@@ -126,39 +136,39 @@ int main(void)
   MX_USART1_UART_Init();
   /* USER CODE BEGIN 2 */
 
-  HAL_GPIO_WritePin(GPIOC, GPIO_PIN_8, GPIO_PIN_SET);
+  HAL_GPIO_WritePin(BLUE_LED_PORT, BLUE_LED_PIN, GPIO_PIN_SET);
   HAL_Delay(1000);
 
   //  If the USER button is not pressed, jump to User Application
-  if (HAL_GPIO_ReadPin(B1_GPIO_Port, B1_Pin) != GPIO_PIN_SET) 
+  if (HAL_GPIO_ReadPin(USER_BTN_PORT, USER_BTN_PIN) != GPIO_PIN_SET)
   {
-	  HAL_GPIO_WritePin(GPIOC, GPIO_PIN_8, GPIO_PIN_RESET);
+	  HAL_GPIO_WritePin(BLUE_LED_PORT, BLUE_LED_PIN, GPIO_PIN_RESET);
 
       // Reset peripherals to guarantee flawless start of user application
 	  PeripheralDeInit();
 
 	  // Jump to user application
-	  BLJumpToUsrApp();
+	  BLJumpToUserApp();
   }
 
   // If USER button is pressed, enter BL Mode to download new firmware program
-  
-  // Erase User App space
+
+  // Erase Flash pages reserved for User Application
   EraseFlashApp();
 
   // Send Ready signal to PC to begin download
-  HAL_Delay(500);
   HAL_UART_Transmit(&huart1, (uint8_t*)begin, sizeof(begin), HAL_MAX_DELAY);
-  HAL_Delay(1000);
 
-  // Receive number of packets from UART
-  // PCK_LEN: 2 digits < 12800 B
-  // PCK_LEN: 3 digits >= 12800 B
+  // PCK_LEN: 2.56 kB <= 2 digits < 25.6 kB
+  // PCK_LEN: 3 digits > 25.6 kB
   for (uint8_t i = 0; i < PCK_LEN; i++)
+  {
+	  // Receive number of packets from UART
 	  HAL_UART_Receive(&huart1, (uint8_t *)&rxPack[i], 1, HAL_MAX_DELAY);
+  }
 
-  // Process number of expected packets
-  packetExpect = TotalPack();
+  // Process number of expected data packets
+  packetExpect = TotalPacket();
 
   // Now host waits for ready bit to send data packets
   HAL_Delay(1000);
@@ -172,17 +182,19 @@ int main(void)
   {
 	  HAL_UART_Receive_IT(&huart1, (uint8_t *)&rxByte, sizeof(rxByte));
 
-	  if (rxCplt == 1) { // Write RAM buffer contents to Flash
-		  rxCplt = 0;
-		  *(ram + rxIndex) = rxByte;
-		  rxIndex += 1;
+	  if (rxCplt == 1) // If receive transaction is complete
+	  {
+		  rxCplt = 0;			// Clear flag
+		  *(ram + rxIndex) = rxByte;	// Store received byte in RAM buffer
+		  rxIndex += 1;			// Increment RAM offset
 
-		  if (rxIndex == CALLOC_SIZE) { // end of packet
-		  	packetIndex++;
-		  	WriteToFlash(rxIndex); // Write RAM buffer contents to Flash
-		  	rxIndex = 0;
+		  if (rxIndex == CALLOC_SIZE) 	// Data packet is filled
+		  {
+		  	packetIndex++;		// Increment the number of packets received
+		  	WriteToFlash(rxIndex); 	// Write RAM buffer contents to Flash
+		  	rxIndex = 0;		// Clear the packet index
+
 		  	// Process complete, send Ready flag
-		  	HAL_Delay(500);
 		  	HAL_UART_Transmit(&huart1, (uint8_t*)ready, sizeof(ready), HAL_MAX_DELAY);
 		  }
 
@@ -190,9 +202,9 @@ int main(void)
 		  __HAL_UART_FLUSH_DRREGISTER(&huart1);
 	  }
 
-	  if (packetIndex >= packetExpect) {
-		  // Soft reset once program flash is complete
-		  NVIC_SystemReset();
+	  if (packetIndex == packetExpect) // All packets have been received
+	  {
+		  NVIC_SystemReset(); // Soft reset once program flash is complete
 	  }
 
     /* USER CODE END WHILE */
@@ -262,7 +274,7 @@ static void MX_USART1_UART_Init(void)
 
   /* USER CODE END USART1_Init 1 */
   huart1.Instance = USART1;
-  huart1.Init.BaudRate = 9600;
+  huart1.Init.BaudRate = 115200;
   huart1.Init.WordLength = UART_WORDLENGTH_8B;
   huart1.Init.StopBits = UART_STOPBITS_1;
   huart1.Init.Parity = UART_PARITY_NONE;
@@ -292,7 +304,6 @@ static void MX_GPIO_Init(void)
 
   /* GPIO Ports Clock Enable */
   __HAL_RCC_GPIOA_CLK_ENABLE();
-  __HAL_RCC_GPIOB_CLK_ENABLE();
   __HAL_RCC_GPIOC_CLK_ENABLE();
 
   /*Configure GPIO pin Output Level */
@@ -303,12 +314,6 @@ static void MX_GPIO_Init(void)
   GPIO_InitStruct.Mode = GPIO_MODE_EVT_RISING;
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   HAL_GPIO_Init(B1_GPIO_Port, &GPIO_InitStruct);
-
-  /*Configure GPIO pin : Pi_Pin */
-  GPIO_InitStruct.Pin = Pi_Pin;
-  GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
-  GPIO_InitStruct.Pull = GPIO_NOPULL;
-  HAL_GPIO_Init(Pi_GPIO_Port, &GPIO_InitStruct);
 
   /*Configure GPIO pins : LD4_Pin LD3_Pin */
   GPIO_InitStruct.Pin = LD4_Pin|LD3_Pin;
@@ -321,23 +326,34 @@ static void MX_GPIO_Init(void)
 
 /* USER CODE BEGIN 4 */
 
+/**
+  * @brief  Relocates Interrupt Vector Table to SRAM
+  * @param  None
+  * @retval None
+  */
 void VectorTableRelocate(void)
 {
 	uint32_t i = 0;
 
-	/* Assign VT to address array in SRAM */
+	// Assign Vector Table to address array in SRAM
 	for (i = 0; i < 48; i++)
-		VectorTable[i] = *(__IO uint32_t*)(FLASH_USR_ADDR + (i<<2));
-
+	{
+		VectorTable[i] = *(__IO uint32_t*)(FLASH_USR_ADDR + (i << 2));
+	}
 }
 
+/**
+  * @brief  De-initializes all used peripherals
+  * @param  None
+  * @retval None
+  */
 void PeripheralDeInit(void)
 {
 	// DeInitialize peripherals in reverse order they were initialized
 	// 1. GPIO
 	HAL_GPIO_DeInit(GPIOA, GPIO_PIN_0);
-	HAL_GPIO_DeInit(GPIOC, GPIO_PIN_9);
-	HAL_GPIO_DeInit(GPIOC, GPIO_PIN_8);
+	HAL_GPIO_DeInit(GREEN_LED_PORT, GREEN_LED_PIN);
+	HAL_GPIO_DeInit(BLUE_LED_PORT, BLUE_LED_PIN);
 	HAL_GPIO_DeInit(GPIOA, GPIO_PIN_14);
 
 	__HAL_RCC_GPIOA_CLK_DISABLE();
@@ -354,45 +370,62 @@ void PeripheralDeInit(void)
 	__disable_irq();
 
 	// 7. Clocks
-	SysTick->CTRL = 0;
-	SysTick->LOAD = 0;
-	SysTick->VAL = 0;
-
+	SysTick->CTRL = 0;	// SysTick Control and Status Register
+	SysTick->LOAD = 0;  // SysTick Reload Value Register
+	SysTick->VAL = 0;	// SysTick Current Value Register
 	__HAL_RCC_SYSCFG_CLK_DISABLE();
-
 	HAL_RCC_DeInit();
 
 	// 8. HAL
 	HAL_DeInit();
 }
 
-void BLJumpToUsrApp(void)
+/**
+  * @brief  Jump from bootloader to user application
+  * @param  None
+  * @retval None
+  */
+void BLJumpToUserApp(void)
 {
 	// function pointer to hold the user app reset handler address
 	void (*AppResetHandler) (void);
 
-	// Configure MSP by reading the value from the base address of sector 3
+	// Configure MSP to the value of the base address of the user flash space
 	uint32_t mspVal = *(volatile uint32_t *) FLASH_USR_ADDR;
 
 	// Set new main stack pointer
 	__set_MSP(mspVal); // From CMSIS
 
-	// Relocate user app Vector Table (N/A M0)
+	// Relocate user app Vector Table (N/A on Cortex-M0)
 	// SCB->VTOR = FLASH_USR_ADDR
+	VectorTableRelocate();
 
 	// Reset handler address is the next 32-bit address after the base (VT)
-	uint32_t resetHandlerAddr = *(volatile uint32_t *) (FLASH_USR_ADDR+4);
+	uint32_t resetHandlerAddr = *(volatile uint32_t *) (FLASH_USR_ADDR + 4);
 	AppResetHandler = (void*) resetHandlerAddr;
 
 	// Jump to the reset handler of the user application
 	AppResetHandler();
 }
 
+/**
+  * @brief  Calculates the number of pages of user app flash to erase
+  * @param  None
+  * @retval Number of pages of flash to erase
+  */
 static inline uint32_t PagesToErase(void)
 {
-	return ((FLASH_BASE_ADDR + (64*1024)) - FLASH_USR_ADDR)/1024;
+	uint32_t numPages;
+	numPages = ((FLASH_BASE_ADDR + (MAX_FLASH_PAGES * 1024)) - FLASH_USR_ADDR) / 1024;
+
+	return numPages;
 }
 
+/**
+  * @brief  Erase the user application portion of flash memory
+  * @param  None
+  * @retval None
+  */
 void EraseFlashApp(void)
 {
 	// Unlock Flash
@@ -400,24 +433,29 @@ void EraseFlashApp(void)
 	HAL_FLASH_OB_Unlock();
 
 	// Erase
-	FLASH_EraseInitTypeDef f;
-	f.TypeErase = FLASH_TYPEERASE_PAGES;
-	f.PageAddress = (uint32_t)FLASH_USR_ADDR;
-	f.NbPages = PagesToErase(); // Calculates usr app space based on page addr
+	FLASH_EraseInitTypeDef userFlash;
+	userFlash.TypeErase = FLASH_TYPEERASE_PAGES;
+	userFlash.PageAddress = (uint32_t)FLASH_USR_ADDR;
+	userFlash.NbPages = PagesToErase(); // Calculates usr app space based on page addr
 	uint32_t pageError = 0;
 
-	HAL_FLASHEx_Erase(&f, &pageError);
+	HAL_FLASHEx_Erase(&userFlash, &pageError);
+	// Clear Flash Erase bit in Flash Control Register
+	CLEAR_BIT(FLASH->CR, FLASH_CR_PER);
 
 	// Lock Flash
 	HAL_FLASH_OB_Lock();
 	HAL_FLASH_Lock();
 }
-// void EraseFlashBL(void)
 
-// Pass ram buffer index
+/**
+  * @brief  Write data to flash memory
+  * @param  Input: RAM buffer index
+  * @retval None
+  */
 void WriteToFlash (uint16_t index)
 {
-	uint8_t i;
+	uint16_t i;
 	static uint32_t offset = 0;
 	static uint32_t *flashAddr = NULL;
 	flashAddr = (uint32_t *) FLASH_USR_ADDR;
@@ -427,18 +465,21 @@ void WriteToFlash (uint16_t index)
 	HAL_FLASH_OB_Unlock();
 
 	/* Burn to flash */
-	uint32_t tempBuf = 0; // only for debug
+	uint32_t tempBuf = 0;
 
 	for (i = 0; i < index;)
 	{
-		//tempBuf = *((uint32_t*) ram + i++);
-		//tempBuf = SWAP_INT32(tempBuf);
-
+		// Shift each data byte into a 32-bit temporary buffer
 		tempBuf |= (*(ram + i++) << 0);
 		tempBuf |= (*(ram + i++) << 8);
 		tempBuf |= (*(ram + i++) << 16);
 		tempBuf |= (*(ram + i++) << 24);
-		HAL_FLASH_Program(FLASH_TYPEPROGRAM_WORD, (uint32_t)(flashAddr+offset), tempBuf);
+
+		// Write the 32-bit word to flash
+		HAL_FLASH_Program(FLASH_TYPEPROGRAM_WORD, (uint32_t)(flashAddr + offset), tempBuf);
+		// Clear PG bit in Flash Control Register
+		CLEAR_BIT(FLASH->CR, FLASH_CR_PG);
+
 		offset++;
 		tempBuf = 0;
 	}
@@ -450,36 +491,51 @@ void WriteToFlash (uint16_t index)
 	// Clear RAM buffer
 	for (i = 0; i < index; i++)
 	{
-		*(ram+i) &= 0;
+		*(ram + i) &= 0;
 	}
 }
 
-// rxPack array converts to single uint32_t
-static inline uint32_t TotalPack(void)
+/**
+  * @brief  Converts array to single uint32_t
+  * @param  None
+  * @retval Number of packets to be received
+  */
+static inline uint32_t TotalPacket(void)
 {
 	uint32_t packets = 0;
-	static uint32_t pow10[4] = {1, 10, 100, 1000};
+	// Lookup table for power of base 10 constants: 10^0
+	static uint32_t pow10[4] = {1, 10, 100, 1000}; // 10^0, 10^1, 10^2,...
 
 	for (uint8_t i = 0; i < PCK_LEN; i++)
 	{
-		if (*(rxPack+i) == 0)
-			packets += (*(rxPack+i))*pow10[(PCK_LEN-1) - i];
+		if (*(rxPack + i) == 0)
+		{
+			packets += (*(rxPack + i)) * pow10[(PCK_LEN - 1) - i];
+		}
 		else
-			packets += (*(rxPack+i) - 48)*pow10[(PCK_LEN-1) - i];
+		{
+			packets += (*(rxPack + i) - 48) * pow10[(PCK_LEN - 1) - i];
+		}
 	}
 
 	return packets;
 }
 
+/**
+  * @brief  UART Receive Complete Interrupt Callback
+  * @param  None
+  * @retval None
+  */
 void HAL_UART_RxCpltCallback(UART_HandleTypeDef *huart)
 {
 	if (huart == &huart1) {
-		HAL_GPIO_WritePin(GPIOC, GPIO_PIN_9, GPIO_PIN_SET);
+		HAL_GPIO_WritePin(GREEN_LED_PORT, GREEN_LED_PIN, GPIO_PIN_SET);
 
 		if (packetIndex <= packetExpect)
+		{
 			rxCplt = 1;
-
-		HAL_GPIO_WritePin(GPIOC, GPIO_PIN_9, GPIO_PIN_RESET);
+		}
+		HAL_GPIO_WritePin(GREEN_LED_PORT, GREEN_LED_PIN, GPIO_PIN_RESET);
 	}
 }
 
