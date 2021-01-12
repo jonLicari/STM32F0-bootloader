313d2fc13a47d43ac47664ff82b4bdfc6d50e8ae
Jonathan Licari <jlicari@ryerson.ca>
2021-01-12 15:50:36 -0500

    Update host.py
    Implement checksum for the entire data file.

--- host/host.py
+++ host/host.py
@@ -1,9 +1,11 @@
-import serial
-import time
+import binascii
 import os
+import serial
+import sys
+import time
 
 # Constants
-fileName = 'cli.bin'
+fileName = 'tv1.bin'
 portNum = 'COM3'
 SIZE = 256
 
@@ -29,12 +31,26 @@ print("Connected to port")
 endFlag = 0
 
 def main():
+    global endFlag
+
     ctr = 0
     while True:
         flag = port.read(1) # Read UART 
         if (flag == b'2'): # Only transmit once STM is ready
             print(flag)
+            flag = 0
             time.sleep(1)
+            
+            # Send the checksum of the firmware file 
+            crc = Checksum(fileName)
+            print(crc)
+            crcDecimal = int(crc, 16)
+            print(crcDecimal)
+            # convert integer to list
+            a = list(map(int, str(crcDecimal)))
+            print(a)
+            port.write(a)
+
             # Send the number of packets to be expected
             NumberOfPackets()
             break
@@ -44,17 +60,43 @@ def main():
         if (flag == b'1'): # Only transmit once STM is ready
             print(flag)
             flag = 0 # reset ready bit
-            transmit()
-            ctr+=1
+            Transmit()
+            ctr += 1
             print(ctr)
+            
+            # EOF handling
             if (endFlag == 1):
-                print("Close port")
-                port.close()
-                break # Exit main() if end of file is reached
-        else:
-            print("Waiting..")
+                endFlag = (endFlag and 0)
+                # Read ack/ nack flag from MCU
+                crcCheckFlag = port.read(1)
+                print("CRC Flag: " + str(crcCheckFlag))
 
-def transmit():
+                if (crcCheckFlag == "Y"):
+                    # Close UART COM Port
+                    print("Close port")
+                    port.close()
+                    break # Exit main() if end of file is reached
+
+                elif (crcCheckFlag == "N"):
+                    # Close UART COM Port
+                    print("Close port")
+                    port.close()
+                    # Checksum did not match, restart update                    
+                    #os.execl(sys.executable, sys.executable, *sys.argv)
+                    break
+                else:
+                    print("Error: Checksum did not match")
+                    port.close()
+                    break 
+
+                break                   
+            
+        #else:
+            #print("Waiting..")
+
+def Transmit():
+    global endFlag
+
     # Send bytes to com port to be received
     print("Begin Transmission")
     index = 0
@@ -63,7 +105,6 @@ def transmit():
         byte = dataFile.read(1) # read 1 byte
 
         if (byte == b''):
-            global endFlag
             endFlag = 1 # Set end of file flag
             port.write(b'\xFF')
         else:
@@ -73,8 +114,7 @@ def transmit():
         
     if (endFlag == 1):
         print("End of file ")
-        # Close file 
-        dataFile.close()
+        dataFile.close() # Close file 
 
     print("End packet ")
 
@@ -85,7 +125,9 @@ def NumberOfPackets():
         packetSize = totalBytes//SIZE
     else:
         packetSize = (totalBytes//SIZE)+1
-
+    
+    print(packetSize)
+    
     # convert integer to list
     a = list(map(int, str(packetSize))) 
     
@@ -102,6 +144,10 @@ def NumberOfPackets():
 
     port.write(array)
 
+def Checksum(fileName):
+    buf = open(fileName,'rb').read()
+    buf = (binascii.crc32(buf) & 0xFFFFFFFF)
+    return "%08X" % buf
 
 if __name__ == "__main__":
     main()
